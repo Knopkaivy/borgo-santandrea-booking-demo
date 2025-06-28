3f432943b1630c656b14c8c5806d7c5d8ff3c655
TIVY-PK\tivy <tetiana.ivy@concentrix.com>
2025-06-28 07:52:23 -0500

    add react-calendar

--- reactdemo.client/src/Components/Booking/CalendarContainer.jsx
+++ reactdemo.client/src/Components/Booking/CalendarContainer.jsx
@@ -1,13 +1,25 @@
 import React, { useState } from "react";
+import Calendar from 'react-calendar';
 import '../../Styles/Booking/CalendarContainer.css';
+import 'react-calendar/dist/Calendar.css';
 
 
 
-function CalendarContainer() {
-    const [selectedDates, setSelectedDates] = useState([]);
+function CalendarContainer({ minDate, maxDate, startDate, endDate, handleDateChange}) {
+
   return (
       <div className='calendar-container' >
           <p className='calendar-container__disclaimer' >We're showing the best available price per room for 1 night, based on the number of guests.</p>
+          <Calendar
+              onChange={handleDateChange}
+              value={[startDate, endDate]}
+              showDoubleView={true}
+              minDate={minDate}
+              maxDate={maxDate}
+              selectRange={true}
+              next2Label={null}
+              prev2Label={null}
+          />
           <p className='calendar-container__warning' ><span className='bold' >Warning</span> goes here</p>
           <div className='calendar-container__buttons-container' >
               <button className="calendar-container__button--transparent" >Cancel</button>
--- reactdemo.client/src/Components/Booking/DatePicker.jsx
+++ reactdemo.client/src/Components/Booking/DatePicker.jsx
@@ -1,19 +1,9 @@
-import React, { useState } from 'react';
 import CalendarIcon from '../Icons/CalendarIcon';
 import '../../Styles/Booking/DatePicker.css';
 
-function DatePicker({ type }) {
+function DatePicker({ date, type }) {
     const title = type === 'check-in' ? 'Check-in' : 'Check-out';
-    let initialDate;
-    const today = new Date();
-    if (type === 'check-in') {
-        initialDate = today.toLocaleDateString();
-    } else {
-        const tomorrow = new Date(today);
-        tomorrow.setDate(today.getDate() + 1);
-        initialDate = tomorrow.toLocaleDateString();
-    }
-    const [date, setDate] = useState(initialDate);
+
   return (
       <div className="date-picker" >
           <div className="date-picker__icon-container">
@@ -24,7 +14,7 @@ function DatePicker({ type }) {
                   {title}
               </div>
               <div className="date-picker__selected-value">
-                  {date}
+                  {date.toLocaleDateString()}
               </div>
           </div>
       </div>
