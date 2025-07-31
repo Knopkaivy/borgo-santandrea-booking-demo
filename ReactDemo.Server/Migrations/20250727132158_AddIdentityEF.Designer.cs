54fe2e2267ac9f0107a6cbdcab713c6941cd3be2
TIVY-PK\tivy <tetiana.ivy@concentrix.com>
2025-07-30 19:02:16 -0500

    migrations

--- ReactDemo.Server/Migrations/20250727132158_AddIdentityEF.Designer.cs
+++ ReactDemo.Server/Migrations/20250727132158_AddIdentityEF.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ReactDemo.Server.Database;
 
@@ -11,9 +12,11 @@ using ReactDemo.Server.Database;
 namespace ReactDemo.Server.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250727132158_AddIdentityEF")]
+    partial class AddIdentityEF
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
